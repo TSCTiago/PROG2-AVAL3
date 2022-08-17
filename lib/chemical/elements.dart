621b68024d64477a9e225f7163e35dbd350bf049
Francisco Mendes <fmmagalhaesfilho@aluno.uespi.br>
2022-08-16 22:02:03 -0300

    Implement basic comands in elements

--- lib/chemical/elements.dart
+++ lib/chemical/elements.dart
@@ -1,3 +1,38 @@
-class Elements{
-  //
+// ignore_for_file: unnecessary_brace_in_string_interps
+
+import 'dart:io';
+
+//símbolo, nome em português, nome em latim e peso atômico.
+class Element {
+  String symbol;
+  String name;
+  String latinName;
+  String weight;
+
+  Element(this.symbol, this.name, this.latinName, this.weight);
+  @override
+  String toString() {
+    return symbol;
+  }
+}
+
+class Elements extends Iterable {
+  final List<Element> _elements = [];
+  final file = File('elements.csv').readAsLinesSync();
+ 
+  // file.readAsLinesSync();
+  Elements() {
+    for (int i = 1; i < file.length; i++) {
+    var li = file[i].split(',');
+      String symbol = li[1];
+      String name = li[2];
+      String latinName = li[3];
+      String weight = li[0];
+      _elements.add(Element(symbol = symbol, name = name, latinName = latinName,
+          weight = weight));
+    }
+  }
+
+  @override
+  Iterator<Element> get iterator => _elements.iterator;
 }
