22fec694d5b6fbda007ee1dce392df45a058d47f
Francisco Mendes <fmmagalhaesfilho@aluno.uespi.br>
2022-08-16 22:55:15 -0300

    refatoring the code

--- lib/molecule.dart
+++ lib/molecule.dart
@@ -9,24 +9,26 @@ class Molecule implements Comparable<Molecule>{
     this.formula = formula;
   }
 
-
   set formula(String formula) {
     if (!formula.isNotEmpty){
       throw Exception('Fórmula vazia');
     }
     _formula = formula;
     _weigth = 0;
+
     RegExp expression = RegExp(r'(?<atom>[A-Z][a-z]?d*)(?<quantity>[0-9]*)');
 
     Iterable<RegExpMatch> molecules = expression.allMatches(formula.toString());
 
     for (final atom in molecules) {
+      
+      int quantity = 1;
       String number = atom.namedGroup('quantity').toString();
+      
       if (number == '1'){
         throw Exception('Fórmula inválida');
       }
-      int quantity = 1;
-
+      
       if (number.isNotEmpty) {
         quantity = int.parse(number);
       }
