c39071cf6d4dd9dca68497e7142e5fe65ec317d8
Tiago Silva <87677785+TSCTiago@users.noreply.github.com>
2022-08-16 22:15:54 -0300

    change elements class

--- lib/chemical/elements.dart
+++ lib/chemical/elements.dart
@@ -1,10 +1,10 @@
-import 'package:prog2_aval3/chemical/elements.dart';
+import 'dart:io';
 
 class Element {
   String symbol;
   String name;
   String latinName;
-  String weight;
+  int weight;
 
   Element(this.symbol, this.name, this.latinName, this.weight);
   @override
@@ -14,22 +14,36 @@ class Element {
 }
 
 class Elements extends Iterable {
-  final List<Element> _elements = [];
-  final file = File('elements.csv').readAsLinesSync();
-
-  Elements() {
+  static final Elements _instance = Elements._privateConstructor();
+  factory Elements() {
+    return _instance;
+  }
+  final List<Element> elements = [];
+  Elements._privateConstructor() {
+    final file = File('elements.csv').readAsLinesSync();
     for (int i = 1; i < file.length; i++) {
       var li = file[i].split(',');
       String symbol = li[1];
       String name = li[2];
       String latinName = li[3];
-      String weight = li[0];
-      _elements.add(Element(symbol = symbol, name = name, latinName = latinName,
+      int weight = int.parse(li[0]);
+      elements.add(Element(symbol = symbol, name = name, latinName = latinName,
           weight = weight));
     }
   }
 
   @override
-  Iterator<Element> get iterator => _elements.iterator;
+  Iterator<Element> get iterator => elements.iterator;
 
+  Element filter(String substance) {
+    try {
+      final listofSelectedElements = Elements()
+          .elements
+          .where((atom) => atom.symbol == substance)
+          .toList();
+      return listofSelectedElements[0];
+    } catch (_) {
+      throw Exception('Elemento não encontrado');
+    }
+  }
 }
